9eba7fb92a96218289528408cec5c854871ff678
unknown <jordan.kay@gmail.com>
2021-03-22 10:07:54 -0600

    moved database to backup

--- parrot/lib/database.py
+++ parrot/lib/database.py
@@ -13,16 +13,21 @@ def get_key_schema(database: pd.DataFrame = None) -> tuple:
     schema = get_schema(database)
     return schema[0], schema
 
+def get_database_path():
+    path = config.get().get('database at', 'default')
+    if path == 'default':
+        return config.root('..', 'database', 'database.csv')
+    return path
 
 def get():
     try:
-        return pd.read_csv(config.root('database', 'database.csv'))
+        return pd.read_csv(get_database_path())
     except Exception as e:
         return pd.DataFrame([], columns=get_schema())
 
 
 def save(database: pd.DataFrame):
-    return database.to_csv(config.root('database', 'database.csv'), index=False)
+    return database.to_csv(get_database_path(), index=False)
 
 
 def upsert(database: pd.DataFrame, record: dict):
